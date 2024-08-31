82619339c0bf9612064ac0bbd63d31fcc2a2bc20
MaryKwawu <may419316@gmail.com>
2024-08-31 16:29:57 +0000

    Feat/button : worked on the contact me and hire me button

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
 import { cva, type VariantProps } from "class-variance-authority";
 import { IoMdClose } from "react-icons/io";
-
 import { cn } from "@/lib/utils";
 
 const Sheet = SheetPrimitive.Root;
@@ -40,7 +39,7 @@ const sheetVariants = cva(
 					"inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
 				left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
 				right:
-					"inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+					"inset-y-0 right-0 h-full w-3/4 data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
 			},
 		},
 		defaultVariants: {
@@ -65,7 +64,7 @@ const SheetContent = React.forwardRef<
 			{...props}
 		>
 			{children}
-			<SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+			<SheetPrimitive.Close className="absolute right-8 top-8 transition-opacity outline-none">
 				<IoMdClose className="text-3xl text-accent" />
 				<span className="sr-only">Close</span>
 			</SheetPrimitive.Close>
